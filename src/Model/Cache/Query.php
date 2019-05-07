f76fcfd62aad9bf82daa73d5068963c02b73f0ba
Ilja Lapkovskis <ilja@scandiweb.com>
2019-05-07 10:37:38 +0300

    Magento cache control for persisted query storage (#3)
    * Add flush option for persisted query redis
    
    * Add cache control for PQ and PQ responses
    
    * Add flushing logic for varnish and redis
    
    * Update README.md
    
    * Remove version tag from composer.json
    
    * Fix code styling and comments

---
+++ src/Model/Cache/Query.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * ScandiPWA_PersistedQuery
+ *
+ * @category    ScandiPWA
+ * @package     ScandiPWA_PersistedQuery
+ * @author      Ilja Lapkovskis <ilja@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ */
+
+namespace ScandiPWA\PersistedQuery\Model\Cache;
+
+
+use Magento\Framework\App\Cache\Type\FrontendPool;
+use Magento\Framework\Cache\Frontend\Decorator\TagScope;
+use ScandiPWA\PersistedQuery\RedisClient;
+
+/**
+ * Class Query
+ * @package ScandiPWA\PersistedQuery\Model\Cache
+ */
+class Query extends TagScope
+{
+    const TYPE_IDENTIFIER = 'PERSISTED_QUERY';
+    
+    const CACHE_TAG = 'PERSISTED_QUERY';
+    
+    /**
+     * @var Response
+     */
+    private $responseCache;
+    
+    /**
+     * Query constructor.
+     * @param FrontendPool $frontendPool
+     * @param RedisClient  $redisClient
+     * @param Response     $responseCache
+     */
+    public function __construct(
+        FrontendPool $frontendPool,
+        RedisClient $redisClient,
+        Response $responseCache
+    )
+    {
+        $this->client = $redisClient;
+        $this->responseCache = $responseCache;
+        parent::__construct($frontendPool->get(self::TYPE_IDENTIFIER), self::CACHE_TAG);
+    }
+    
+    /**
+     * @param string $mode
+     * @param array  $tags
+     * @return bool
+     */
+    public function clean($mode = \Zend_Cache::CLEANING_MODE_ALL, array $tags = [])
+    {
+        $varnishPurge = $this->responseCache->clean();
+        $redisFlush = $this->client->flushDb();
+        $redisFlush = $redisFlush == 'OK';
+        
+        return $varnishPurge && $redisFlush;
+    }
+    
+}
---
+++ src/Model/PurgeCache.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ScandiPWA_PersistedQuery
+ *
+ * @category    ScandiPWA
+ * @package     ScandiPWA_PersistedQuery
+ * @author      Ilja Lapkovskis <ilja@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ */
+
+namespace ScandiPWA\PersistedQuery\Model;
+
+use Magento\CacheInvalidate\Model\PurgeCache as CorePurgeCache;
+
+/**
+ * Class PurgeCache
+ * @package ScandiPWA\PersistedQuery\Model
+ */
+class PurgeCache extends CorePurgeCache
+{
+    /**
+     * @param string $poolTag
+     * @return bool
+     */
+    public function sendPurgeRequest($poolTag): bool
+    {
+        $socketAdapter = $this->socketAdapterFactory->create();
+        $servers = $this->cacheServer->getUris();
+        $socketAdapter->setOptions(['timeout' => 10]);
+        $headers = ['X-Pool' => $poolTag];
+        
+        foreach ($servers as $server) {
+            $headers['Host'] = $server->getHost();
+            try {
+                $socketAdapter->connect($server->getHost(), $server->getPort());
+                $socketAdapter->write(
+                    'PURGE',
+                    $server,
+                    '1.1',
+                    $headers
+                );
+                $socketAdapter->read();
+                $socketAdapter->close();
+            } catch (\Exception $e) {
+                echo "Error!! ";
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
