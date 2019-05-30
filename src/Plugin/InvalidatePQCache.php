5739cb6efdda13eb8ddd414086938dc00ab3facb
Ilja Lapkovskis <ilja@scandi.com>
2019-05-30 15:42:00 +0300

    Invalidate PQ cache status on reindex

---
+++ src/Plugin/InvalidatePQCache.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * ScandiPWA_CatalogGraphQl
+ *
+ * @category    ScandiPWA
+ * @package     ScandiPWA_Cache
+ * @author      Ilja Lapkovskis <ilja@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ */
+
+namespace ScandiPWA\PersistedQuery\Plugin;
+
+use Magento\Catalog\Model\Indexer\Category\Product\AbstractAction;
+use Magento\Framework\App\Cache\StateInterface;
+use Magento\Framework\App\Cache\TypeListInterface;
+use ScandiPWA\PersistedQuery\Cache\Response as ResponseCache;
+
+class InvalidatePQCache
+{
+    /**
+     * @var bool
+     */
+    private $cacheState;
+    
+    /**
+     * @var TypeListInterface
+     */
+    protected $typeList;
+    
+    /**
+     * @param StateInterface    $cacheState
+     * @param TypeListInterface $typeList
+     */
+    public function __construct(
+        TypeListInterface $typeList,
+        StateInterface $cacheState
+    )
+    {
+        $this->cacheState = $cacheState->isEnabled(strtolower(ResponseCache::CACHE_TAG));
+        $this->typeList = $typeList;
+    }
+    
+    /**
+     * @param AbstractAction $subject
+     * @param AbstractAction $result
+     * @return AbstractAction
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterExecute(AbstractAction $subject, AbstractAction $result)
+    {
+        if ($this->cacheState) {
+            $this->typeList->invalidate('persisted_query_response');
+        }
+        return $result;
+    }
+}
